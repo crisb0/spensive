7724b6dba946e607f1e9fbd89e769b212c9621b0
crisb0 <christine.tvbui@gmail.com>
2018-03-02 00:48:28 +1100

    added basic routing but not rly

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,66 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
 import logo from './money-bag.svg';
 import './Home.css';
 import { Sidebar, Segment, Menu, Icon, Button, Image } from 'semantic-ui-react';
+
+const routes = [
+	{
+		path: "/",
+		exact: true
+	},
+	{
+		path: "/shoelaces",
+		component: Shoelaces
+	}
+];
+
+const Shoelaces = () => <p>shoelaces page</p>;
 
 class Navbar extends Component {
 		constructor(props) {
 			super(props);
 		}
-		handleClick(event) {
-			console.log("Item was clicked");
-		}
+
+
+
 	  render() {
 	    return (
+				<Router>
 	      <div>
-	        <Menu inverted stackable>
-	          <Menu.Item><Image src={logo} size="mini"/></Menu.Item>
+	        <Menu inverted stackable className='PageMargins'>
+	          <Menu.Item><Link to="/"><Image src={logo} size="mini"/></Link></Menu.Item>
 	          <Menu.Item><h1>SPENSIVE</h1></Menu.Item>
 	          <Menu.Menu position='right'>
-						<Menu.Item name='home' onClick={() => this.handleClick()}/>
-							<Menu.Item name='account summary' onClick={() => this.handleClick()}/>
+						<Menu.Item name='home'><NavLink to="/">Home</NavLink></Menu.Item>
+							<Menu.Item name='account summary'><NavLink to="/shoelaces">Account Summary</NavLink></Menu.Item>
 	          </Menu.Menu>
 	        </Menu>
 	      </div>
+				</Router>
 		);
   }
 }
 
-export default Navbar;
+class HomeBody extends Component {
+	render() {
+		return(
+			<div classname="PageMargins">
+				<p>hello!!!</p>
+			</div>
+		);
+	}
+}
+
+class App extends Component {
+	render() {
+		return(
+			<div>
+				<Navbar />
+				<HomeBody />
+			</div>
+		);
+	}
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import './index.css';
 import Navbar from './Home';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Navbar />, document.getElementById('root'));
+ReactDOM.render(
+  <Navbar />, document.getElementById('root')
+);
 registerServiceWorker();
